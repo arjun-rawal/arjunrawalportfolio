9b387181a7a058d53ea6d9f715164c678bd2b276
arjun-rawal <arawalarjun@gmail.com>
2023-01-15 13:35:32 -0500

    site

---
+++ src/app/aboutMe/page.tsx
@@ -0,0 +1,35 @@
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import portrait from '../assets/homeArjun.jpg'
+import styles from '../../app/page.module.css'
+import '../../app/globals.css'
+import bg from '../assets/patternBackground.png'
+import Link from 'next/link'
+export default function Home() {
+
+  return (
+    <body className={styles.everything}>
+  <main className={styles.main}>
+    <div className={styles.menuDiv}>
+      <div className={styles.menuBar}>
+      <Link href="/contact" className={styles.menuButtons}>
+          Contact
+      </Link>
+      <Link href="/projects" className={styles.menuButtons}>       
+          Projects
+      </Link>
+      <Link href="/aboutMe" className={styles.menuButtons}>
+          About Me
+      </Link>
+      <Link href="/" className={styles.menuButtons}>
+          Home 
+      </Link>
+      </div>
+    </div>
+     
+    </main>
+    </body>
+
+
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,76 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
+import portrait from '../assets/homeArjun.jpg'
 import styles from './page.module.css'
+import '../app/globals.css';
+import bg from '../assets/patternBackground.png'
+import Link from 'next/link'
+import { useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [isShown, setIsShown] = useState(false);
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    
+    <body className={styles.everything}>
+  <main className={styles.main}>
+
+    <div className={styles.menuDiv}>
+
+      <div className={styles.menuBar}>
+
+      <Link href="/contact" className={styles.menuButtons}>
+          Contact
+      </Link>
+      <div onMouseLeave={()=> setIsShown(false)} onMouseEnter={() => setIsShown(true)} className={styles.dropDownButton}>
+        <a onClick={()=>setIsShown(!isShown)} className={styles.menuButtons}>       
+            Projects
+        </a>
+        {isShown && (
+          <>
+        <Link href="" className={styles.menuButtons} >
+          Businesses
+        </Link>
+        <Link href="" className={styles.menuButtons} >
+          Computer Science
+        </Link>
+        </>
+        )}
+      </div>
+      <Link href="/aboutMe" className={styles.menuButtons}>
+          About Me
+      </Link>
+      <Link href="/" className={styles.menuButtons}>
+          Home 
+      </Link>
+
+      </div>
+    </div>
+      <div className={styles.texts}>
+        <div className={styles.description}>
+          <h1 className={styles.name}>
+            Arjun Rawal
+          </h1>
+            <h3 className={styles.headline}>
+              Passion for Computer Science and Education
+            </h3>
+            {/* <Image 
+            className={styles.homeImage}
+            src={portrait}
+            alt="Picture of Arjun"
+            width={506.7}
+            height={722.34}
+            />      */}
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+    </body>
+
+
   )
 }
