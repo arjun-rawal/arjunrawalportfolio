aa506990ef6c25cbd4aee556fb6d50ecb5704999
arjun-rawal <arawalarjun@gmail.com>
2023-01-16 15:59:41 -0500

    added animations to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,25 @@ import '../app/globals.css';
 import bg from '../assets/patternBackground.png'
 import Link from 'next/link'
 import { useState } from 'react';
+import { motion, useViewportScroll, useTransform } from "framer-motion";
 
 
 export default function Home() {
 
-
+  const { scrollYProgress } = useViewportScroll()
+  const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
 
   return (
     
     <body className={styles.everything}>
     <main className={styles.main}>
       <div className={styles.menuDiv}>
-        <div className={styles.menuBar}>
+        
+        <motion.div initial="hidden" animate="visible" variants={{hidden: {scale: 0.2,opacity: 0},visible: {scale: 1,opacity: 1,transition: {delay: 1.5}},}}className={styles.menuBar}>
         <Link href="/contact" className={styles.menuButtons}>
             Contact
         </Link>
+        
         <Link href="/projects" className={styles.menuButtons}>       
             Projects
         </Link>
@@ -31,16 +35,20 @@ export default function Home() {
         <Link href="/" className={styles.menuButtons}>
             Home 
         </Link>
-        </div>
+        </motion.div>
       </div>
       <div className={styles.texts}>
         <div className={styles.description}>
+        <motion.div initial="hidden" animate="visible" variants={{hidden: {scale: 0.2,opacity: 0},visible: {scale: 1,opacity: 1,transition: {delay: 0.5}},}}>
           <h1 className={styles.name}>
             Arjun Rawal
           </h1>
+          </motion.div>
+          <motion.div initial="hidden" animate="visible" variants={{hidden: {scale: 0.2,opacity: 0},visible: {scale: 1,opacity: 1,transition: {delay: 1}},}}>
             <h3 className={styles.headline}>
               Passion for Computer Science and Education
             </h3>
+          </motion.div>
             {/* <Image 
             className={styles.homeImage}
             src={portrait}
@@ -50,6 +58,7 @@ export default function Home() {
             />      */}
         </div>
       </div>
+      
 
     </main>
     </body>
